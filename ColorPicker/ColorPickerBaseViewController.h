ab1f1921d38a8bee9a0d92a0df111e9c460f0540
lynnliu <lynn.liuzechen@gmail.com>
2013-02-28 09:56:31 +0800

    wx

--- ColorPicker/ColorPickerBaseViewController.h
+++ ColorPicker/ColorPickerBaseViewController.h
@@ -6,6 +6,12 @@
 //  Copyright (c) 2013 uLynn. All rights reserved.
 //
 
+#define AdKey @"56OJzXIIuNaulQzdc5"
+#define ITUNESURL @"https://itunes.apple.com/us/app/color-picker-for-developer/id608956277?ls=1&mt=8"
+
+#define WXAppID @"wx2a062d4628d88879"
+#define WXAppKey @"8ce6b0aeff1396c98028e8d59a95ae12"
+
 #import <UIKit/UIKit.h>
 
 @interface ColorPickerBaseViewController : UIViewController
